5cc12766e48e8619ad52c80c9116071bd973e161
parithi star <elamparithi804@gmail.com>
2022-08-10 03:19:48 -0700

    finilize code for the signup login and authentication

--- server/user/modals/signup-model.js
+++ server/user/modals/signup-model.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const signupSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true,
+        unique: true
         
     }
 });
--- server/user/utility.js
+++ server/user/utility.js
@@ -1,23 +1,11 @@
-const signupModal = require("./modals/signup-modal");
-const bcrypt = require("bcryptjs");
-const checkExistingUser = async (email)=> {
+const signupModal=require("./modals/signup-model")
+const checkExistingUser = async (email)=>{
     let existingUser = false;
-    await signupModal.find({email: email}).then((userData)=> {
-        if(userData.length) {
-            existingUser = true;
+    await signupModal.find({email: email}).then((userData)=>{
+        if(userData.length){
+            existingUser=true;
         }
-    });
-    return existingUser;
+    })
+    return existingUser
 }
-
-const generatePasswordHash = (password) => {
-    const salt = 10;
-    return new Promise((resolve, reject)=> {
-         bcrypt.genSalt(salt).then((hashSalt)=> {
-            bcrypt.hash(password, hashSalt).then((passwordHash)=> {
-                resolve(passwordHash);
-            })
-        })
-    });
-}
-module.exports = {checkExistingUser, generatePasswordHash};
+module.exports =checkExistingUser
