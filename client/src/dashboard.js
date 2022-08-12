c5cdfe18ffe9512f29a03e8b9fae9a6c36006c05
parithi star <elamparithi804@gmail.com>
2022-08-12 07:47:03 -0700

    dashboard changes

--- client/src/dashboard.js
+++ client/src/dashboard.js
@@ -2,10 +2,11 @@ import "./dashboard.css"
 import Vector from "./asserts/Vectorpng.png"
 import Vector_1 from "./asserts/Vectorvector-1.png"
 import Vector_2 from "./asserts/Vector-1.png"
+import Vector_3 from "./asserts/Vector.png"
 
 function Dashboard() {
   return (
-   <div className="post">
+    <div className="post">
     <div className="sidebar">
       <div className="logo-2">
         LOGO
@@ -20,7 +21,21 @@ function Dashboard() {
         <img src={Vector_2} alt ="vector-2" className="vector-2"></img>
 
       </div>
+      <div className="Logout">
+        <img src={Vector_3} alt="Logout" className="Logout-1"></img>
+        <div className="Logout-2">Logout</div>
+      </div>
 
+    </div>
+    <div className="Header">
+      <div className="tittle-2">
+        Total Contacts
+        </div>
+        <input type="search" placeholder="Search by EmailId..." className="Searchbar"></input>
+
+    </div>
+    <div className="table">
+      
     </div>
    </div>
     
